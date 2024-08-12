a43c3b26f79e43b8ff1e34fdd5a23f275f51c35d
Leelavathy <leelavathy@N13847.intellectdesign.com>
2024-08-12 10:37:12 +0530

    initial commit

---
+++ src/Result.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Result({ secretNumber, term }) {
+
+    let result;
+    if (term) {
+        if (term > secretNumber) {
+            result = 'Higher'
+
+        } else if (term < secretNumber) {
+            result = 'Lower'
+        } else if (term == secretNumber) {
+            result = 'Yipee Correct'
+        } else {
+            result = 'Enter valid input'
+        }
+    }
+
+
+
+    return (
+        <h3>You guessed: {result}</h3>
+    )
+}
+
+export default Result;
